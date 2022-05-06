b968424e13f8c73b1479254af1b245c785de893c
zhouzihong <aqua5201314@outlook.com>
2022-05-06 04:58:10 -0700

    finish lab1

---
+++ libsponge/stream_reassembler.cc
@@ -0,0 +1,127 @@
+#include "stream_reassembler.hh"
+
+#include <cassert>
+
+// Dummy implementation of a stream reassembler.
+
+// For Lab 1, please replace with a real implementation that passes the
+// automated checks run by `make check_lab1`.
+
+// You will need to add private members to the class declaration in `stream_reassembler.hh`
+
+template <typename... Targs>
+void DUMMY_CODE(Targs &&.../* unused */) {}
+
+using namespace std;
+
+StreamReassembler::StreamReassembler(const size_t capacity)
+    : _unassemble_strs()
+    , _next_assembled_idx(0)
+    , _unassembled_bytes_num(0)
+    , _eof_idx(-1)
+    , _output(capacity)
+    , _capacity(capacity) {}
+
+//! \details This function accepts a substring (aka a segment) of bytes,
+//! possibly out-of-order, from the logical stream, and assembles any newly
+//! contiguous substrings and writes them into the output stream in order.
+void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
+    auto pos_iter = _unassemble_strs.upper_bound(index);
+    if (pos_iter != _unassemble_strs.begin())
+        pos_iter--;
+
+    size_t new_idx = index;
+    if (pos_iter != _unassemble_strs.end() && pos_iter->first <= index) {
+        const size_t up_idx = pos_iter->first;
+
+        // 如果当前子串前面出现了重叠
+        if (index < up_idx + pos_iter->second.size())
+            new_idx = up_idx + pos_iter->second.size();
+    }
+    // 如果前面没有子串，则和当前读取到的pos进行比较
+    else if (index < _next_assembled_idx)
+        new_idx = _next_assembled_idx;
+
+    // 子串新起始位置对应到的 data 索引
+    const size_t data_start_pos = new_idx - index;
+
+    ssize_t data_size = data.size() - data_start_pos;
+
+    pos_iter = _unassemble_strs.lower_bound(new_idx);
+
+    // 如果和下面的子串冲突，则截断长度
+    while (pos_iter != _unassemble_strs.end() && new_idx <= pos_iter->first) {
+        const size_t data_end_pos = new_idx + data_size;
+        // 如果存在重叠区域
+        if (pos_iter->first < data_end_pos) {
+            if (data_end_pos < pos_iter->first + pos_iter->second.size()) {
+                data_size = pos_iter->first - new_idx;
+                break;
+            }
+            else {
+                _unassembled_bytes_num -= pos_iter->second.size();
+                pos_iter = _unassemble_strs.erase(pos_iter);
+                continue;
+            }
+        }
+        // 如果不重叠则直接 break
+        else
+            break;
+    }
+
+    // 判断是否还有数据是独立的， 顺便检测当前子串是否被上一个子串完全包含
+    if (data_size > 0) {
+        const string new_data = data.substr(data_start_pos, data_size);
+        // 如果新字串可以直接写入
+        if (new_idx == _next_assembled_idx) {
+            const size_t write_byte = _output.write(new_data);
+            _next_assembled_idx += write_byte;
+            if (write_byte < new_data.size()) {
+                // _output 写不下了，插入进 _unassemble_strs 中
+                const size_t can_store_byte =
+                    std::min(_capacity - _unassembled_bytes_num, new_data.size() - write_byte);
+                if (can_store_byte) {
+                    const string data_to_store = new_data.substr(write_byte, can_store_byte);
+                    _unassembled_bytes_num += data_to_store.size();
+                    _unassemble_strs.insert(make_pair(_next_assembled_idx, std::move(data_to_store)));
+                }
+            }
+        } else {
+            const size_t can_store_byte = std::min(_capacity - _unassembled_bytes_num, new_data.size());
+            if (can_store_byte) {
+                const string data_to_store = new_data.substr(0, can_store_byte);
+                _unassembled_bytes_num += data_to_store.size();
+                _unassemble_strs.insert(make_pair(new_idx, std::move(data_to_store)));
+            }
+        }
+    }
+
+    // 一定要处理重叠字串的情况
+    for (auto iter = _unassemble_strs.begin(); iter != _unassemble_strs.end(); /* nop */) {
+        assert(_next_assembled_idx <= iter->first);
+        if (iter->first == _next_assembled_idx) {
+            const size_t write_num = _output.write(iter->second);
+            _next_assembled_idx += write_num;
+            if (write_num < iter->second.size()) {
+                _unassembled_bytes_num += iter->second.size() - write_num;
+                _unassemble_strs.insert(make_pair(_next_assembled_idx, std::move(iter->second.substr(write_num))));
+
+                _unassembled_bytes_num -= iter->second.size();
+                _unassemble_strs.erase(iter);
+                break;
+            }
+            _unassembled_bytes_num -= iter->second.size();
+            iter = _unassemble_strs.erase(iter);
+        }
+        else
+            break;
+    }
+    if (eof)
+        _eof_idx = index + data.size();
+    if (_eof_idx <= _next_assembled_idx)
+        _output.end_input();
+}
+
+size_t StreamReassembler::unassembled_bytes() const { return _unassembled_bytes_num; }
+
+bool StreamReassembler::empty() const { return _unassembled_bytes_num == 0; }
